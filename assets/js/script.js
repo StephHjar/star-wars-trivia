02f466e1d7e1858c88b02d07545caec8ce535f2c
Steph Hjartarson <stephanie.hja@gmail.com>
2022-05-05 20:32:47 +0000

    Make question and answers appear in div and buttons when the game is initiated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
-document.addEventListener("click", runGame);
+document.addEventListener("click", startGame);
+
+let currentQuestion = 0;
+var score = 0;
+
+let questions = [{
+    "question": "What is Luke Skywalker’s wife Mara’s last name?",
+    "options": ["Ruby", "Jade", "Garnet"],
+    "answer": "Jade"
+}, {
+    "question": "Tenel Ka Djo is princess and eventually queen of what planet?",
+    "options": ["Hapes", "Dathomir", "Yavin IV"],
+    "answer": "Hapes"
+}]
+
+
+function startGame() {
+    document.getElementsByClassName("content")[0].style.display = "block";
+    runGame();
+}
 
 function runGame() {
-    document.getElementsByClassName("content")[0].style.display = "block";
+    currentQuestion = 0;
+    document.getElementsByClassName("question-area")[0].innerHTML = questions[currentQuestion].question;
+    document.getElementsByClassName("btn-a")[0].innerHTML = questions[currentQuestion].options[0];
+    document.getElementsByClassName("btn-b")[0].innerHTML = questions[currentQuestion].options[1];
+    document.getElementsByClassName("btn-c")[0].innerHTML = questions[currentQuestion].options[2];
+    console.log("Testing")
 }
 
 function displayQuestion() {
