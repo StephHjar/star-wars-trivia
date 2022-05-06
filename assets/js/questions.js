f69a0b2c89415ef6dfcf7ae1f9bc694044d17d82
Steph Hjartarson <stephanie.hja@gmail.com>
2022-05-06 19:47:24 +0000

    Add functions to have questions appear in  a random order with no repeated questions, separate questions into their own JS file

---
+++ assets/js/questions.js
@@ -0,0 +1,41 @@
+export let questions = [{
+    "question": "What is Luke Skywalker’s wife Mara’s last name?",
+    "options": ["Ruby", "Jade", "Garnet"],
+    "answer": "Jade"
+}, {
+    "question": "Tenel Ka Djo is princess and eventually queen of what planet?",
+    "options": ["Hapes", "Dathomir", "Yavin IV"],
+    "answer": "Hapes"
+}, {
+    "question": "Timothy Zahn’s trilogy of novels that were credited with reigniting interest in the Star Wars Expanded Universe are called the ______ Trilogy.",
+    "options": ["Vader", "Pellaeon", "Thrawn"],
+    "answer": "Thrawn"
+}, {
+    "question": "What is Jacen Solo's Sith name?",
+    "options": ["Darth Bane", "Darth Caedus", "Darth Sidious"],
+    "answer": "Darth Caedus"
+}, {
+    "question": "Which of the Solo children does Chewbacca save before being killed?",
+    "options": ["Jaina", "Anakin", "Jacen"],
+    "answer": "Anakin"
+}, {
+    "question": "In 'The Lando Calrissian Adventures', what is the name of Lando’s droid companion?",
+    "options": ["Vuffi Raa", "Em Teedee", "Thon Baka"],
+    "answer": "Vuffi Raa"
+}, {
+    "question": "What are the Yuuzhan Vong’s ships made of?",
+    "options": ["stone", "metal", "coral"],
+    "answer": "coral"
+}, {
+    "question": "In ‘The Truce at Bakura’, who is arrested for sedition?",
+    "options": ["Leia", "Luke", "Han"],
+    "answer": "Leia"
+}, {
+    "question": "Who is one of Leia’s closest friends, and caretaker of her children?",
+    "options": ["Winter", "Autumn", "Summer"],
+    "answer": "Winter"
+}, {
+    "question": "Who does Jaina Solo marry?",
+    "options": ["Zekk", "Kyp Durron", "Jagged Fel"],
+    "answer": "Jagged Fel"
+}];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,84 @@
-document.addEventListener("click", startGame);
+ import {
+     questions
+ } from "./questions.js";
 
-let buttons = document.getElementsByTagName("button");
+ let currentQuestion = null;
+ let buttons = document.getElementsByTagName("button");
+ let CURRENT_QUESTION_COUNT = 0
+ let MAX_QUESTION = 10;
 
-for (let button of buttons) {
-    button.addEventListener("click", checkAnswer);
-}
+ document.addEventListener("click", startGame);
 
-let questions = [{
-    "question": "What is Luke Skywalker’s wife Mara’s last name?",
-    "options": ["Ruby", "Jade", "Garnet"],
-    "answer": "Jade"
-}, {
-    "question": "Tenel Ka Djo is princess and eventually queen of what planet?",
-    "options": ["Hapes", "Dathomir", "Yavin IV"],
-    "answer": "Hapes"
-}, {
-    "question": "Timothy Zahn’s trilogy of novels that were credited with reigniting interest in the Star Wars Expanded Universe are called the ______ Trilogy.",
-    "options": ["Vader", "Pellaeon", "Thrawn"],
-    "answer": "Thrawn"
-}, {
-    "question": "What is Jacen Solo's Sith name?",
-    "options": ["Darth Bane", "Darth Caedus", "Darth Sidious"],
-    "answer": "Darth Caedus"
-}, {
-    "question": "Which of the Solo children does Chewbacca save before being killed?",
-    "options": ["Jaina", "Anakin", "Jacen"],
-    "answer": "Anakin"
-}, {
-    "question": "In 'The Lando Calrissian Adventures', what is the name of Lando’s droid companion?",
-    "options": ["Vuffi Raa", "Em Teedee", "Thon Baka"],
-    "answer": "Vuffi Raa"
-}, {
-    "question": "What are the Yuuzhan Vong’s ships made of?",
-    "options": ["stone", "metal", "coral"],
-    "answer": "coral"
-}, {
-    "question": "In ‘The Truce at Bakura’, who is arrested for sedition?",
-    "options": ["Leia", "Luke", "Han"],
-    "answer": "Leia"
-}, {
-    "question": "Who is one of Leia’s closest friends, and caretaker of her children?",
-    "options": ["Winter", "Autumn", "Summer"],
-    "answer": "Winter"
-}, {
-    "question": "Who does Jaina Solo marry?",
-    "options": ["Zekk", "Kyp Durron", "Jagged Fel"],
-    "answer": "Jagged Fel"
-}]
+ let repeatedQuestion = []
 
-let currentQuestion = questions[questions.length * Math.random() | 0];
 
-/**
- * Initiates gameplay when the player clicks anywhere on the page
- */
-function startGame() {
-    document.getElementsByClassName("content")[0].style.display = "block";
-    runGame();
-}
 
-function runGame() {
-    for (let i = 0; i < questions.length; i++) {
-        document.getElementsByClassName("question-area")[0].innerHTML = currentQuestion.question;
-        document.getElementsByClassName("btn-a")[0].innerHTML = currentQuestion.options[0];
-        document.getElementsByClassName("btn-b")[0].innerHTML = currentQuestion.options[1];
-        document.getElementsByClassName("btn-c")[0].innerHTML = currentQuestion.options[2];
-    }
-}
 
-function checkAnswer() {
-    let answer = currentQuestion.answer;
-    let response = this.innerText;
-    console.log(answer);
-    console.log(response);
-    if (answer === response) {
-        incrementScore();
-        this.style.backgroundColor = "green";
-        runGame();
-    } else {
-        console.log("Sorry, you got it wrong!");
-        this.style.backgroundColor = "red";
-        runGame();
-    }
-}
+ /**
+  * Initiates gameplay when the player clicks anywhere on the page
+  */
+ function startGame() {
+     document.getElementsByClassName("content")[0].style.display = "block";
+     renderNextQuestion();
+ }
 
-/**
- * Increases the score by one for every correct answer
- */
-function incrementScore() {
-    let oldScore = parseInt(document.getElementsByClassName("score")[0].innerText);
-    document.getElementsByClassName("score")[0].innerText = ++oldScore;
-}
+ function renderNextQuestion() {
 
-function displayResult() {
+     if (CURRENT_QUESTION_COUNT >= MAX_QUESTION) {
+         return false;
+     }
+     currentQuestion = getRandomQuestion()
+     document.getElementsByClassName("question-area")[0].innerHTML = currentQuestion.question;
+     document.getElementsByClassName("btn-a")[0].innerHTML = currentQuestion.options[0];
+     document.getElementsByClassName("btn-b")[0].innerHTML = currentQuestion.options[1];
+     document.getElementsByClassName("btn-c")[0].innerHTML = currentQuestion.options[2];
+     handleClickEvent();
+ }
 
-}
+ function getRandomQuestion() {
+     currentQuestion = questions[questions.length * Math.random() | 0];
+     if (repeatedQuestion.indexOf(currentQuestion) >= 0) {
+         return getRandomQuestion()
+     }
+     repeatedQuestion.push(currentQuestion)
+     return currentQuestion;
+ }
 
-function shareResult() {
+ function handleClickEvent() {
+     for (let button of buttons) {
+         button.addEventListener("click", checkAnswer);
+     }
+ }
 
-}
+
+ function checkAnswer() {
+     let answer = currentQuestion.answer;
+     let response = this.innerText;
+     console.log(answer);
+     console.log(response);
+     if (answer === response) {
+         incrementScore();
+         this.style.backgroundColor = "green";
+         renderNextQuestion();
+     } else {
+         console.log("Sorry, you got it wrong!");
+         this.style.backgroundColor = "red";
+         renderNextQuestion();
+     }
+ }
+
+ /**
+  * Increases the score by one for every correct answer
+  */
+ function incrementScore() {
+     let oldScore = parseInt(document.getElementsByClassName("score")[0].innerText);
+     document.getElementsByClassName("score")[0].innerText = ++oldScore;
+ }
+
+ function displayResult() {
+
+ }
+
+ function shareResult() {
+
+ }
